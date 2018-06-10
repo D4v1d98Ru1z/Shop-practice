d25d066e4f50407ae74e22ed804d9f899e6ccdd2
David <davidl.ruizc@gmail.com>
2018-06-10 12:47:05 -0500

    finish routing on ts

--- Shop-Pract/src/app/app.module.ts
+++ Shop-Pract/src/app/app.module.ts
@@ -1,5 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
+  import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+//Routes
+import { app_routing } from "./app.routes";
 
 import { AppComponent } from './app.component';
 import { IndexComponent } from './componentes/index/index.component';
@@ -12,7 +15,8 @@ import { ProductComponent } from './componentes/product/product.component';
     ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Shop-Pract/src/app/app.routes.ts
+++ Shop-Pract/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import{ RouterModule, Routes } from "@angular/router";
+import { IndexComponent, ProductComponent } from "./componentes/index.paginas";
 
 const app_routes: Routes = [
-    {path: '', component: Compomemt },
-    { path: '**', pathMatch: 'full', redirectTo: '' }
+    {path: 'index', component: IndexComponent },
+    { path: 'product', component: ProductComponent },
+    { path: '', pathMatch: 'full', redirectTo: 'index' }
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes);
