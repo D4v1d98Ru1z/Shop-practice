b009018c9cf2fd23dd68bf76c2a2ed47db1a52e0
David Ruiz <davidl.ruizc@gmail.com>
2018-06-13 00:05:16 -0500

    service public

--- Shop-Pract/src/app/app.component.ts
+++ Shop-Pract/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-// import {MainService } from "./service/main.service";
+import {MainService } from "./service/main.service";
 
 @Component({
   selector: 'app-root',
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor( ){
+  constructor(public serv:MainService ){
     
   }
 }
