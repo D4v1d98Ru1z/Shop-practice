eec5fca47b31c6358b6040154107bd471a0092d2
David Ruiz <davidl.ruizc@gmail.com>
2018-06-14 15:17:45 -0500

    background from firebase

--- Shop-Pract/src/app/componentes/product/product.component.ts
+++ Shop-Pract/src/app/componentes/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from "../../service/product.service";
 
+import { DomSanitizer } from '@angular/platform-browser';
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -9,11 +11,14 @@ import { ProductService } from "../../service/product.service";
 export class ProductComponent implements OnInit {
 
   public Producto;
-  constructor(private ps:ProductService) { }
+  constructor(private ps:ProductService, private img:DomSanitizer) {}
+
+  getBackground(image) {
+    return this.img.bypassSecurityTrustStyle(`url(${image})`);
+}
 
   ngOnInit() {
     this.ps.ProductData().subscribe(data =>{
-      console.log(data.json());
       this.Producto = data.json();
     });
   }
