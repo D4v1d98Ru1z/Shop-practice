27565be345a340e2755d398cb5e713d266f90523
David Ruiz <davidl.ruizc@gmail.com>
2018-06-16 16:02:11 -0500

    export components

--- Shop-Pract/src/app/componentes/index.paginas.ts
+++ Shop-Pract/src/app/componentes/index.paginas.ts
@@ -1,2 +1,6 @@
 export { IndexComponent } from "./index/index.component";
 export { ProductComponent } from "./product/product.component";
+export { SingleComponent } from "./single/single.component";
+export { AboutComponent } from "./about/about.component";
+export { ContactComponent } from "./contact/contact.component";
+export { ServicesComponent } from "./services/services.component";
